02244fe8f272b0395f225695ff63a108dca7d6fd
البرمجة مهند القريني <programming2014@hotmail.com>
2019-02-04 20:22:51 +0300

    Add DB

--- SQL_LocalDB/Form1.cs
+++ SQL_LocalDB/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,30 @@ namespace SQL_LocalDB
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static SqlConnection conn = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; AttachDbFilename = |DataDirectory|\DB1.mdf; Integrated Security = True");
+
+        private  bool OpenConn()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(OpenConn())
+            {
+                MessageBox.Show("OK");
+            }
         }
     }
 }
